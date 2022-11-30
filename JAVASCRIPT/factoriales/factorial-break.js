9ef552d170072fa1abb5eec2f8b315bef13ee891
Miguel Ángel Ruiz <73854124+miguelruizF@users.noreply.github.com>
2022-11-29 23:10:44 -0600

    archivos factorial

---
+++ JAVASCRIPT/factoriales/factorial-break.js
@@ -0,0 +1,10 @@
+let x = 1;
+let result = 1;
+while(x<10){
+    if(x == 0){
+        break;
+    }
+    result*=x;
+    x++;
+}
+console.log(result);
---
+++ JAVASCRIPT/factoriales/factorial-while.js
@@ -0,0 +1,7 @@
+let x = 1;
+let result = 1;
+while(x<10){
+    result*=x;
+    x++;
+}
+console.log(result);
