ec23f595816378e59b1ac2163a886c897645e428
Miguel Ruiz <later481@gmail.com>
2022-12-05 21:16:39 -0600

    numbers

---
+++ numbers/numbers.js
@@ -0,0 +1,11 @@
+let altura_cm = parseInt(1.70);
+let altura_m = parseFloat(1.7);
+let peso_float = parseFloat(65);
+let altura_arriba = 1.69;
+console.log(Math.round(altura_arriba));
+
+let peso_abajo = 64.4;
+console.log(Math.floor(peso_abajo));
+
+let mayor = Number.MAX_VALUE;
+console.log(mayor + 1);
