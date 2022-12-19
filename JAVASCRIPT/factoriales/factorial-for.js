a8d6f92cc84647f931740008a7f71d0bb1eb68af
Miguel Ruiz <later481@gmail.com>
2022-12-18 21:57:42 -0600

    sets

---
+++ JAVASCRIPT/factoriales/factorial-for.js
@@ -0,0 +1,7 @@
+// Factorial
+let result = 1;
+for(let i = 1; i <= 10; i++){
+    result = result*i;
+}
+console.log(result);
+
---
+++ JAVASCRIPT/sets y objetos/sets/sets.js
@@ -0,0 +1,9 @@
+let fam = ["Pedro", "Claudia", "Gloria", "Maribel", "Miguel"]
+const familia = new Set(fam);
+console.log(familia);
+
+familia.add("Miguel");
+console.log(familia);
+
+familia.add("Javascript");
+console.log(familia);
