187357cb15d7b488edd7e3e47a86789bfdbbf7ba
Miguel Ruiz <later481@gmail.com>
2022-12-26 22:03:42 -0600

    import & export

---
+++ JAVASCRIPT/modulos-node/import-oper/controlling.js
@@ -0,0 +1,10 @@
+export const suma = (a, b) => {
+    return a+b;
+};
+
+export const multiplica = (a, b) => {
+    if(a>0 || b>0){
+        return a*b
+    }
+    return 1;
+}
---
+++ JAVASCRIPT/modulos-node/index.js
@@ -0,0 +1,8 @@
+import {suma, multiplica} from "./import-oper/controlling.js";
+import chalk from "chalk";
+
+const suma1 = suma(5,3);
+console.log(suma1)
+
+const multi = multiplica(5,2);
+console.log(chalk.bgGreen(multi));
